74278597d17253d660ddfd27bf94da6234608c0d
Marco Bispo <marcobispo97@gmail.com>
2026-01-23 19:45:14 -0300

    :construction: Doing: L_49

--- S5_Class_attributes_define_shared/Code+-+Class+Attributes/2 - Backpack (Class Attribute).py
+++ S5_Class_attributes_define_shared/Code+-+Class+Attributes/2 - Backpack (Class Attribute).py
@@ -3,7 +3,9 @@ Course: Python OOP - Object Oriented Programming for Beginners
 By: Estefania Cassingena Navone
 """
 
-class House:
-    
-    def __init__(self, price):
-        self.price = price
+class Backpack:
+
+    max_num_items = 10
+
+    def __init__(self):
+        self.items = []
--- S5_Class_attributes_define_shared/Code+-+Class+Attributes/8 - Backpack Class (Update Class Attribute).py
+++ S5_Class_attributes_define_shared/Code+-+Class+Attributes/8 - Backpack Class (Update Class Attribute).py
@@ -5,11 +5,18 @@ By: Estefania Cassingena Navone
 
 class Backpack:
 
+    max_num_items = 10
+
     def __init__(self):
         self.items = []
-        print(self.items)
 
+
+print(Backpack.max_num_items)
 
 my_backpack = Backpack()
+print(my_backpack.max_num_items)
 
-print(my_backpack.items)
+Backpack.max_num_items = 15
+
+print(Backpack.max_num_items)
+print(my_backpack.max_num_items)
